2b138bed2adbba739e4390d9582ce8989d6536bc
filipe silva <filipe.silva@conject.com>
2016-05-11 14:22:14 +0200

    Changed Makefiles to create a build folder at make time and delete it at cleaning time

---
+++ asm/helloworld/main.asm
@@ -0,0 +1,24 @@
+; MO5 Test stuff
+
+	ORG $3000
+		; Could be a little lower ?
+
+main
+	; Disable interrupts
+;	ORCC #$50
+
+;-----------------------------------------------------
+; Color Computer with 6809
+;-------------------------------------------------------
+
+START   LDX     #MESG   POINT TO MESSAGE
+        BSR     PRINT   PRINT MESSAGE
+        RTS
+
+PRN10   JSR     [$A002] PRINT CHARACTER IN A
+PRINT   LDA     ,X+     GET NEXT CHARACTER
+        BNE     PRN10   IF NOT 0, PRINT IT
+        RTS
+
+MESG    FCC     'HELLO WORLD!'
+        FCB     0
